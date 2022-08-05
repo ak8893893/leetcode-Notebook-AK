d46148314b3675ad9a521afa77af39db62ffa209
ak8893893 <88812653+ak8893893@users.noreply.github.com>
2022-08-05 12:12:26 +0800

    Create 13. Roman to Integer.py

---
+++ 13. Roman to Integer.py
@@ -0,0 +1,95 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        Ans = 0
+        Temp = ""
+        for x in s:
+            if Temp != "I" and Temp != "X" and Temp != "C":
+                if x == "I":
+                    Ans += 1
+                    Temp = x
+                elif x == "V":
+                    Ans += 5
+                    Temp = x
+                elif x == "X":
+                    Ans += 10
+                    Temp = x
+                elif x == "L":
+                    Ans += 50
+                    Temp = x
+                elif x == "C":
+                    Ans += 100
+                    Temp = x
+                elif x == "D":
+                    Ans += 500
+                    Temp = x
+                elif x == "M":
+                    Ans += 1000
+                    Temp = x
+            elif Temp == "I":
+                if x == "I":
+                    Ans += 1
+                    Temp = x
+                elif x == "V":
+                    Ans += 3
+                    Temp = x
+                elif x == "X":
+                    Ans += 8
+                    Temp = x
+                elif x == "L":
+                    Ans += 50
+                    Temp = x
+                elif x == "C":
+                    Ans += 100
+                    Temp = x
+                elif x == "D":
+                    Ans += 500
+                    Temp = x
+                elif x == "M":
+                    Ans += 1000
+                    Temp = x
+            elif Temp == "X":
+                if x == "I":
+                    Ans += 1
+                    Temp = x
+                elif x == "V":
+                    Ans += 5
+                    Temp = x
+                elif x == "X":
+                    Ans += 10
+                    Temp = x
+                elif x == "L":
+                    Ans += 30
+                    Temp = x
+                elif x == "C":
+                    Ans += 80
+                    Temp = x
+                elif x == "D":
+                    Ans += 500
+                    Temp = x
+                elif x == "M":
+                    Ans += 1000
+                    Temp = x
+            elif Temp == "C":
+                if x == "I":
+                    Ans += 1
+                    Temp = x
+                elif x == "V":
+                    Ans += 5
+                    Temp = x
+                elif x == "X":
+                    Ans += 10
+                    Temp = x
+                elif x == "L":
+                    Ans += 50
+                    Temp = x
+                elif x == "C":
+                    Ans += 100
+                    Temp = x
+                elif x == "D":
+                    Ans += 300
+                    Temp = x
+                elif x == "M":
+                    Ans += 800
+                    Temp = x
+
+        return Ans
