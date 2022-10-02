c3eb36bcce78b6a5edad0950cce45c04fd00cfea
ak <ak8893893@yahoo.com.tw>
2022-10-02 18:15:04 +0800

    correct

--- problems/0091. Decode Ways.py
+++ problems/0091. Decode Ways.py
@@ -1,31 +1,33 @@
 """
-Title : 0804. Unique Morse Code Words
+Title : 0091. Decode Ways
 Author : AK 賴韋銘
-Time : 2022/08/17
+Time : 2022/10/2
+refer :
+https://www.youtube.com/watch?v=VCJsUYeuqaY
+https://zh.wikipedia.org/wiki/%E6%96%90%E6%B3%A2%E9%82%A3%E5%A5%91%E6%95%B0
+https://leetcode.com/problems/decode-ways/discuss/2645738/Python-Dp-solutoin-or-98-Fast-or-Space-Optimised
 """
 
 class Solution:
-    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+    def numDecodings(self, s: str) -> int:
 
-        # 建立英文字列表
-        alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-        
-        # 建立摩斯密碼表
-        wordMorse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
-        
-        # 建立空的集合 存放取得的摩斯密碼型態
-        finalSet = set()
-        
-        # 把輸入列表中的每個字逐一比對轉換成摩斯密碼後放入集合
-        for word in words:
-            transformation = ""
-            for w in word:
-                i = 0
-                for z in alphabet:
-                    if z == w: break
-                    i += 1
-                transformation += wordMorse[i]
-            finalSet.add(transformation)
-        
-        # 回傳集合長度(代表取得多少種不同的形態)
-        return len(finalSet)
+        # make a dp list with len(s)+1 of 0
+        dp=[0 for _ in range(len(s)+1)]
+
+        # first 0 is not valid so if s[0] > 0 let dp[0] and dp[1] = 1
+        if int(s[0])>0:
+            dp[0]=1
+            dp[1]=1
+
+        # i start from 2 to len(s)
+        for i in range(2,len(s)+1):
+            # if current is 0 then it alone can't be mapped
+            if int(s[i-1])>0:
+                dp[i]=dp[i-1]
+            # combining previous 2
+            x=s[i-2:i]
+            if 10<=int(x)<=26: # if this is b/w bounds then it can be mapped
+                dp[i]+=dp[i-2]
+
+        return dp[len(s)]
+print(Solution.numDecodings(self="",s="1202"))
