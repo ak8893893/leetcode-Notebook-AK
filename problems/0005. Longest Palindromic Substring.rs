b98bc4c9663f1be152127a9651d5132e393183ba
AK <ak8893893@yahoo.com.tw>
2024-03-12 15:47:03 +0800

    Update 0005. Longest Palindromic Substring.rs

--- problems/0005. Longest Palindromic Substring.rs
+++ problems/0005. Longest Palindromic Substring.rs
@@ -5,6 +5,7 @@ String::from_utf8(...)：這是一個 Rust 函數，它將字節切片轉換為 
 如果切片中的字節不是有效的 UTF-8，則 from_utf8() 會返回一個錯誤，指示為什麼該切片不是有效的 UTF-8。
 .unwrap_or("".to_string())：這部分是處理錯誤的一種方式。如果 from_utf8() 成功將切片轉換為字符串，則 .unwrap_or(...) 會返回該字符串。如果轉換失敗（例如，切片不是有效的 UTF-8），則它會返回一個空字符串（""）。
 目的是將字節切片轉換為字符串。如果切片是有效的 UTF-8，則返回該字符串；否則返回一個空字符串。
+Big O(n^2) window(n), slice(n), rev(1)
 */
 impl Solution {
         pub fn longest_palindrome(s: String) -> String {
@@ -12,14 +13,14 @@ impl Solution {
         while window_size > 0 {          
              match s.as_bytes()                     // 將字符串轉換為字節切片 
                     .windows(window_size)           // 創造長度為目前窗口大小的滑動窗口  滑動窗口會創造長度為窗口大小的 slice 的數個切片每個起始都向前一個差一
-                    .find(|slice| { let iter = slice.iter();   // 把切片變成迭代器
+                    .find(|slice| { let iter = slice.iter();                // 把切片變成迭代器
                                         iter.clone().eq(iter.clone().rev()) // 把迭代的切片和反過來的自己做比較
                                 }
                         ) {
                              Some(slice) => return String::from_utf8(slice.to_vec()).unwrap_or("".to_string()),   // 如果找到了這個切片  回傳這個切片的字串型態  
-                             None => window_size -= 1, // 如果這個大小的窗口長度沒有答案   把窗口大小減小一點再試一次
+                             None => window_size -= 1,                      // 如果這個大小的窗口長度沒有答案   把窗口大小減小一點再試一次
                         }
         }
-        "".to_string() // 如果窗口長度為0表示沒字串  回傳空字串
+        "".to_string()                                                      // 如果窗口長度為0表示沒字串  回傳空字串
     }
 }
